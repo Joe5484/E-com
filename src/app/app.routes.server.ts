2f4115d8175b484dd505478e95edc2290dfad627
Joe5484 <yatef0540@gmail.com>
2026-03-30 02:05:03 +0200

    fix dynamic routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,24 @@
+// import { RenderMode, ServerRoute } from '@angular/ssr';
+
+// export const serverRoutes: ServerRoute[] = [
+//   {
+//     path: '**',
+//     renderMode: RenderMode.Prerender
+//   }
+// ];
+
+
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  {
+    path: 'products-details/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'chackout/:cartId',
+    renderMode: RenderMode.Server
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
